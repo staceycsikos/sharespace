4b2159578d278453a52eaaf598179e320e6fd2f5
Stacey Csikos <stacey@staceys-mbp.myfiosgateway.com>
2022-05-27 16:58:10 -0400

    fixed typo in granting all priv.

--- create-database.sql
+++ create-database.sql
@@ -2,4 +2,4 @@ CREATE DATABASE sharespace;
 
 CREATE USER share_admin WITH PASSWORD 'password';
 
-GRANT ALL PRIVILEGES ON DATABASE sharespace TO sharespace_admin;
+GRANT ALL PRIVILEGES ON DATABASE sharespace TO share_admin;
