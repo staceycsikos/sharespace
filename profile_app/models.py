fe3f2419c273b91f41263162bc731a19cc17697f
Iffat Hossain <iffathossain@iffats-air.mynetworksettings.com>
2022-05-27 23:37:23 -0400

    adds createdb file, profile_app, settings changes

---
+++ profile_app/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+
+  user = models.OneToOneField(User, on_delete=models.CASCADE, null=False)
+  about = models.TextField(max_length=500, blank=True)
+  location = models.CharField(max_length=128, blank=True)
+  birthday = models.DateField(null=True, blank=True)
+  hobbies = models.TextField(max_length=500, blank=True)
+  profession = models.CharField(max_length=128, blank=True)
+  socialmedia = models.URLField(max_length=500, blank=True, unique=True )  
+
+  def __str__(self):
+    return self.user.username
---
+++ profile_app/views.py
@@ -0,0 +1,10 @@
+from rest_framework import viewsets, permissions
+
+from .serializers import UserSerializer
+
+from django.contrib.auth.models import User
+
+class UserViewSet(viewsets.ModelViewSet):
+  queryset = User.objects.all()
+  serializer_class = UserSerializer
+
